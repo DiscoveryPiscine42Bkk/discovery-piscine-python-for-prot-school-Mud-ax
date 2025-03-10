d6758d6828a3b0b42c02f47ac340275489b6143e
Mud-ax <Korndanai.ho@gmail.com>
2025-03-10 13:34:31 +0700

    Add files via upload

---
+++ cell01/ex01/name.py
@@ -0,0 +1,8 @@
+
+def print_full_name(first_name, last_name):
+    full_name = f"{first_name} {last_name}"
+    print(f"Full name: {full_name}")
+
+first_name = "Korndanai"
+last_name = "Ho"
+print_full_name(first_name, last_name)
